94b27ee08c43ee6775e56e1f29484654ec4205dd
Daniel Merys <dkmerys@gmail.com>
2020-04-20 11:26:19 -0700

    add setup for tests

--- __tests__/pandemic-clone.test.js
+++ __tests__/pandemic-clone.test.js
@@ -5,28 +5,32 @@ import { City } from '../src/pandemic-clone.js'
 
 describe('Pandemic', () => {
   jest.useFakeTimers();
+  let pandemic;
+  let player;
+  let virus;
+  let city;
+
+  beforeEach(function() {
+    player = new Player("Scientist")
+    virus = new Virus(10,10);
+    city = new City(100);
+    pandemic = new Pandemic(player, virus, city);
+  });
 
   afterEach(function(){
     jest.clearAllTimers();
   });
 
   test('should create new player object', () => {
-    let pandemic = new Pandemic();
     expect(pandemic.person).toEqual()
   })
   test('should create new Virus with base number of 10', () => {
-    let virus = new Virus(10, 10);
-    let player = new Player("Scientist");
-    let city = new City(10);
+  
     expect(player.profession).toEqual("Scientist")
     expect(virus.infected).toEqual(10)
-    expect(city.population).toEqual(10)
+    expect(city.population).toEqual(100)
   })
   test('virus infected to increase to 15 after 60000 milliseconds', () => {
-    let player = new Player("Scientist")
-    let virus = new Virus(10,10);
-    let city = new City(100);
-    let pandemic = new Pandemic(player, virus, city);
     pandemic.setInfected();
     jest.advanceTimersByTime(60001);
     expect(pandemic.virus.infected).toEqual(15);
