282618b5403b3e605ecd9c65f69aa09da9ec25b4
Daniel Merys <dkmerys@gmail.com>
2020-04-21 09:07:19 -0700

    add createResearch function

--- src/pandemic-clone.js
+++ src/pandemic-clone.js
@@ -25,6 +25,10 @@ export class Pandemic {
       this.city.infected -= 3;
     }, 60000);
   }
+
+  createResearch() {
+    this.virus.cure += 1
+  };
   
   win() {
     if (this.virus.cure >= 5) {
